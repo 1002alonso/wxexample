05dc1ab3b59ab8a4ae715a6a64c0d00997feff69
1002alonso <1002alonso@gmail.com>
2021-12-01 12:17:42 -0600

    fix: 🐛 asdasd
    asdsadasa
    
    BREAKING CHANGE: 🧨 asd
    
    ✅ Closes: www

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -20,7 +20,7 @@ export class MyComponent {
   /**
    * The last name
    */
-  @Prop()  last: string;
+           @Prop()  last: string;
 
   private getText(): string {
     return format(this.first, this.middle, this.last);
